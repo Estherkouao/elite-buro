2139c02e71d4de0124f3f174116946a0371d43de
Esther Kouao <estherkouao9@gmail.com>
2026-08-08 22:31:10 +0000

    changement2

--- config/settings.py
+++ config/settings.py
@@ -276,6 +276,9 @@ SECRET_KEY = 'django-insecure-v^p72(a913s)d*l0m7hxo#gb@md=ej29+c6vafhw1m@6x$(iv8
 DEBUG = True
 
 ALLOWED_HOSTS = [
+     "trusting-wu.164-132-167-236.plesk.page",
+    "localhost",
+    "127.0.0.1",
     
 ]
 
